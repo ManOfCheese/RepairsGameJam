a5cc3be227f5e5e1cec7f6c182d91df9825d2410
ManOfCheese <36536353+ManOfCheese@users.noreply.github.com>
2020-02-01 00:07:01 +0100

    Added directional arrows for stick spawning.

---
+++ Assets/Scripts/ConnectionJoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ConnectionType {
+	Fixed,
+	Hinge
+}
+
+public class ConnectionJoint : MonoBehaviour {
+
+	public Rigidbody rigidBody;
+	public ConnectionType connectionType;
+	public GameObject fixedStick; //stick prefab
+	public GameObject hingeStick; //stick prefab
+
+	public void CreateNewStick( Transform arrowTransform ) {
+		GameObject newStick = null;
+		if ( connectionType == ConnectionType.Fixed ) {
+			newStick = Instantiate( fixedStick );
+		}
+		else if ( connectionType == ConnectionType.Hinge ) {
+			newStick = Instantiate( hingeStick );
+		}
+		if (newStick != null){
+			float halfObjectLength = newStick.GetComponent<MeshRenderer>().bounds.extents.y + 0.5f;
+			newStick.transform.position = new Vector3( arrowTransform.position.x * halfObjectLength, arrowTransform.position.y * halfObjectLength, arrowTransform.position.z * halfObjectLength );
+			newStick.transform.rotation = arrowTransform.rotation;
+		}
+	}
+
+}
---
+++ Assets/Scripts/DirectionalArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalArrow : AClickable {
+
+	private ConnectionJoint connectionPoint;
+
+	private void Start() {
+		connectionPoint = GetComponentInParent<ConnectionJoint>();
+	}
+
+	public override void OnClick() {
+		base.OnClick();
+		connectionPoint.CreateNewStick( transform );
+	}
+}
