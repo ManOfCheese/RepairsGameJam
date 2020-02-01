22a1183ac02fc600b912917aec7ead97c4cf6906
CupcakeMyDucktape <36536272+CupcakeMyDucktape@users.noreply.github.com>
2020-02-01 23:41:43 +0100

    Added Sound to most of the items.

---
+++ Assets/Scripts/RandomizeList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeList : MonoBehaviour {
+
+    //List of the prefabs from the different objects
+    public GameObject[] randomizedObjects;
+
+    void Start() {
+        int randomIndex = Random.Range(0, randomizedObjects.Length);
+
+        GameObject instantiatedObject = Instantiate(randomizedObjects[randomIndex], 0, Quaternion.identity) as GameObject;
+    }
+
+
+    void Update() {
+        
+    }
+}
