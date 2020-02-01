46393d0997153a0500dd55016ed1bf8617b96e64
eMsylf <bobjeltes@hotmail.com>
2020-02-01 22:13:15 +0100

    [+] Added typewriter effect

--- Assets/Scripts/WorldSpaceTextBox.cs
+++ Assets/Scripts/WorldSpaceTextBox.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WorldSpaceTextBox : MonoBehaviour
 {
@@ -27,16 +28,40 @@ public class WorldSpaceTextBox : MonoBehaviour
     [Range(0f, 1f)]
     [SerializeField] private float rotationAmountZ;
 
+    [Header("Text Type Writer")]
+    public TextMeshProUGUI TextComponent;
+    public int CharactersPerFrame = 1;
+    [TextArea]
+    public string SourceText;
+    [TextArea]
+    public string OnScreenText;
+    public char CurrentCharacter;
+
+    public int TextLength;
+    public int CurrentCharacterNumber;
+
+    private void Start()
+    {
+        LoadNewText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    }
+
     void Update()
+    {
+        SwayRotation();
+        TypeWriteText();
+    }
+
+    void SwayRotation()
     {
         if (Rotate)
         {
             time += Time.deltaTime;
-            rotationX = rotationAmountX * Mathf.Sin(time - Mathf.PI /2f);
-            rotationY = rotationAmountY * Mathf.Sin(time - Mathf.PI /2f);
-            rotationZ = rotationAmountZ * Mathf.Sin(time - Mathf.PI /2f);
+            rotationX = rotationAmountX * Mathf.Sin(time - Mathf.PI / 2f);
+            rotationY = rotationAmountY * Mathf.Sin(time - Mathf.PI / 2f);
+            rotationZ = rotationAmountZ * Mathf.Sin(time - Mathf.PI / 2f);
             CanvasHolder.transform.Rotate(rotationX, rotationY, rotationZ);
-        } else
+        }
+        else
         {
             CanvasHolder.transform.rotation = Quaternion.identity;
         }
@@ -61,5 +86,35 @@ public class WorldSpaceTextBox : MonoBehaviour
         newScale.x = Mathf.Pow(CanvasHolder.transform.localScale.x, -1) * TextScaleFinal;
         newScale.y = Mathf.Pow(CanvasHolder.transform.localScale.y, -1) * TextScaleFinal;
         WorldSpaceCanvas.transform.localScale = newScale;
+    }
+
+    public void LoadNewText(string text)
+    {
+        CurrentCharacterNumber = 0;
+        SourceText = text;
+    }
+
+    public void TypeWriteText()
+    {
+        if (string.IsNullOrWhiteSpace(SourceText))
+        {
+            Debug.Log("No text to typewrite");
+            return;
+        }
+        if (CurrentCharacterNumber < SourceText.Length)
+        {
+            OnScreenText += SourceText[CurrentCharacterNumber];
+            CurrentCharacterNumber++;
+            if (TextComponent != null)
+            {
+                TextComponent.text = OnScreenText;
+            }
+        }
+    }
+
+    public void SkipTypeWrite()
+    {
+        OnScreenText = SourceText;
+        CurrentCharacterNumber = SourceText.Length;
     }
 }
