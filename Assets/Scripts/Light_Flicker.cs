f6490928763287de1b9596f91c967a40c5e2b4c3
Carl <jancarlpeters@gmail.com>
2020-02-01 13:07:26 +0100

    Environment aangemaakt en opgezet

---
+++ Assets/Scripts/Light_Flicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Flicker : MonoBehaviour
+{
+    public int[] intRange;
+    public int cutoff;
+
+    Light streetlamp;
+
+
+    void Start()
+    {
+        streetlamp = this.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+    }
+}
