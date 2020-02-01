2e9d09079aa8966fc7830c701d73d839d9bfe9ef
ManOfCheese <36536353+ManOfCheese@users.noreply.github.com>
2020-02-01 10:48:13 +0100

    Start on toggle functionality and joint fixes.

--- Assets/Scripts/ConnectionJoint.cs
+++ Assets/Scripts/ConnectionJoint.cs
@@ -7,12 +7,15 @@ public enum ConnectionType {
 	Hinge
 }
 
-public class ConnectionJoint : MonoBehaviour {
+public class ConnectionJoint : ADeletable {
 
 	public ConnectionType connectionType;
 	public Rigidbody attachedToStick;
+	public GameObject jointPrefab;
+	public GameObject endItemPrefab;
 	public GameObject fixedStick; //stick prefab
 	public GameObject hingeStick; //stick prefab
+	public bool isAnchorPoint;
 
 	private float jointOffset = 1f;
 
@@ -20,18 +23,57 @@ public class ConnectionJoint : MonoBehaviour {
 		GameObject newStick = null;
 		if ( connectionType == ConnectionType.Fixed ) {
 			newStick = Instantiate( fixedStick );
-			newStick.GetComponent<FixedJoint>().connectedBody = attachedToStick;
+			newStick.GetComponent<FixedJoint>().connectedBody = myRigidBody;
 		}
 		else if ( connectionType == ConnectionType.Hinge ) {
 			newStick = Instantiate( hingeStick );
-			newStick.GetComponent<HingeJoint>().connectedBody = attachedToStick;
+			newStick.GetComponent<HingeJoint>().connectedBody = myRigidBody;
 		}
 		if (newStick != null){
 			float halfObjectLength = newStick.GetComponent<MeshRenderer>().bounds.extents.y;
 
 			newStick.transform.rotation = arrowTransform.rotation;
-			newStick.transform.position = ( ( transform.position + ( arrowTransform.position - transform.position ) * jointOffset ) ) + new Vector3( ( arrowTransform.position.x - transform.position.x ) * halfObjectLength, ( arrowTransform.position.y - transform.position.y ) * halfObjectLength, arrowTransform.position.z );
+			newStick.transform.position = ( ( transform.position + ( arrowTransform.position - transform.position ) * jointOffset ) ) + 
+				new Vector3( ( arrowTransform.position.x - transform.position.x ) * halfObjectLength, ( arrowTransform.position.y - transform.position.y ) * halfObjectLength, arrowTransform.position.z );
+
+			//Create standard fixed joint.
+			Instantiate( jointPrefab, transform.position + ( transform.position - attachedToStick.transform.position ).normalized * jointOffset, transform.rotation );
+			ConnectionJoint connectionJoint = jointPrefab.GetComponent<ConnectionJoint>();
+			connectionJoint.connectionType = ConnectionType.Fixed;
+			connectionJoint.GetComponent<FixedJoint>().connectedBody = newStick.gameObject.GetComponent<Rigidbody>();
 		}
 	}
 
+	public void ToggleJoint() {
+		if ( connectionType == ConnectionType.Fixed ) {
+			connectionType = ConnectionType.Hinge;
+		}
+		else if ( connectionType == ConnectionType.Hinge ) {
+			connectionType = ConnectionType.Fixed;
+		}
+	}
+
+	public void CreateEndItem() {
+		Instantiate( jointPrefab, transform.position + ( transform.position - attachedToStick.transform.position ).normalized * jointOffset, transform.rotation );
+		EndItem endItem = jointPrefab.GetComponent<EndItem>();
+		if ( isAnchorPoint ) {
+			endItem.attachedToStick = myRigidBody;
+		}
+		else {
+			endItem.attachedToStick = attachedToStick;
+		}
+		Delete();
+	}
+
+	private void CreateJoint() {
+		Instantiate( jointPrefab, transform.position + ( transform.position - attachedToStick.transform.position ).normalized * jointOffset, transform.rotation );
+		ConnectionJoint connectionJoint = jointPrefab.GetComponent<ConnectionJoint>();
+		connectionJoint.connectionType = ConnectionType.Fixed;
+		if ( isAnchorPoint ) {
+			connectionJoint.attachedToStick = myRigidBody;
+		}
+		else {
+			connectionJoint.attachedToStick = attachedToStick;
+		}
+	}
 }
