db4b228ab6f5a57e8978f6de6c0b51e838f53af1
eMsylf <bobjeltes@hotmail.com>
2020-02-01 21:36:43 +0100

    [/] New version of the text box using artist model [/] Added exposed rotation variables

--- Assets/Scripts/WorldSpaceTextBox.cs
+++ Assets/Scripts/WorldSpaceTextBox.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class WorldSpaceTextBox : MonoBehaviour
 {
+    public Transform CanvasHolder;
     public Canvas WorldSpaceCanvas;
 
-    [Range(.01f, 10f)]
+    [Range(.01f, 50f)]
     public float TextScale;
+    [Range(.1f, 10f)]
+    public float TextScaleMultiplier = 1f;
+    public float TextScaleFinal;
 
+    [Header("Runtime rotation")]
     public bool Rotate;
+    [SerializeField] private float time = 0f;
+    [Space]
+    [SerializeField] private float rotationX;
+    [Range(0f, 1f)]
+    [SerializeField] private float rotationAmountX;
+    [SerializeField] private float rotationY;
+    [Range(0f, 1f)]
+    [SerializeField] private float rotationAmountY;
+    [SerializeField] private float rotationZ;
+    [Range(0f, 1f)]
+    [SerializeField] private float rotationAmountZ;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //OffsetFromAttachTarget = transform.position - WorldSpaceCanvas.transform.position;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //WorldSpaceCanvas.transform.position = transform.position + OffsetFromAttachTarget;
         if (Rotate)
         {
-            transform.Rotate(0f, 1f, 0f);
+            time += Time.deltaTime;
+            rotationX = rotationAmountX * Mathf.Sin(time - Mathf.PI /2f);
+            rotationY = rotationAmountY * Mathf.Sin(time - Mathf.PI /2f);
+            rotationZ = rotationAmountZ * Mathf.Sin(time - Mathf.PI /2f);
+            CanvasHolder.transform.Rotate(rotationX, rotationY, rotationZ);
+        } else
+        {
+            CanvasHolder.transform.rotation = Quaternion.identity;
         }
     }
 
@@ -39,10 +54,12 @@ public class WorldSpaceTextBox : MonoBehaviour
             Debug.LogError("Assign a canvas", this);
             return;
         }
-        WorldSpaceCanvas.GetComponent<RectTransform>().sizeDelta = transform.localScale / TextScale;
+        TextScaleFinal = TextScale * TextScaleMultiplier;
+
+        WorldSpaceCanvas.GetComponent<RectTransform>().sizeDelta = CanvasHolder.transform.localScale / TextScaleFinal;
         Vector3 newScale = Vector3.one;
-        newScale.x = Mathf.Pow(transform.localScale.x, -1) * TextScale;
-        newScale.y = Mathf.Pow(transform.localScale.y, -1) * TextScale;
+        newScale.x = Mathf.Pow(CanvasHolder.transform.localScale.x, -1) * TextScaleFinal;
+        newScale.y = Mathf.Pow(CanvasHolder.transform.localScale.y, -1) * TextScaleFinal;
         WorldSpaceCanvas.transform.localScale = newScale;
     }
 }
