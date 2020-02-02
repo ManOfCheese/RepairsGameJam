7f4c210c55a2d358efe5c0fed5f2333af9647cc0
ManOfCheese <36536353+ManOfCheese@users.noreply.github.com>
2020-02-02 13:37:34 +0100

    Final Version

--- Assets/Scripts/WorldSpaceTextBox.cs
+++ Assets/Scripts/WorldSpaceTextBox.cs
@@ -7,6 +7,7 @@ public class WorldSpaceTextBox : MonoBehaviour
 {
     public Transform CanvasHolder;
     public Canvas WorldSpaceCanvas;
+	public Levels levels;
 
     [Range(.01f, 50f)]
     public float TextScale;
@@ -42,7 +43,7 @@ public class WorldSpaceTextBox : MonoBehaviour
 
     private void Start()
     {
-        LoadNewText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        LoadNewText( levels.LevelList[ 0 ].Mission );
     }
 
     void Update()
